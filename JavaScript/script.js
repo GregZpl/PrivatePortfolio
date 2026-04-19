22874b8be1cbf94d163bf1c5e1ee4aea54bab260
GregZpl <grzegorzpiatek2008@gmail.com>
2026-04-19 19:20:29 +0100

    tried fixing email stuff

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,15 +1,18 @@
 document.getElementById("contactForm").addEventListener("submit", function(e) {
   e.preventDefault();
 
-  emailjs.sendForm(
-    "service_bh6za57",
-    "template_61255uw",
-    this
-  ).then(() => {
-    alert("Message sent successfully!");
-    this.reset();
-  }).catch((error) => {
-    alert("Failed to send message.");
-    console.error(error);
-  });
+  const serviceID = "service_bh6za57";
+
+  const mainEmail = emailjs.sendForm(serviceID, "template_61255uw", this); // TO ME
+  const autoReply = emailjs.sendForm(serviceID, "template_hquf0r8", this); // TO USER
+
+  Promise.all([mainEmail, autoReply])
+    .then(() => {
+      alert("Message sent successfully!");
+      this.reset();
+    })
+    .catch((error) => {
+      alert("Failed to send message.");
+      console.error("EmailJS error:", error);
+    });
 });
